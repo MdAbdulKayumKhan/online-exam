26ea19d391fe9dc1afcd563b441c014a771612bd
MdAbdulKayumKhan <kayum261@gmail.com>
2021-10-04 03:55:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Courses from './components/Courses/Courses';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import NavItems from './components/NavItems/NavItems';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <NavItems></NavItems>
+        <Switch>
+          <Route exact path="/">
+              <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/courses">
+            <Courses></Courses>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavItems/NavItems.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo/logo.jpg';
+
+const NavItems = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <div className="container-fluid">
+                    <Link to="" className="navbar-brand"><img src={logo} alt="" width="40" height="34" /></Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-4 ">
+                            <li className="nav-item">
+                                <Link to="/home" className="nav-link active text-white">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/courses" className="nav-link active text-white">Courses</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/about" className="nav-link active text-white">About</Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavItems;
