57a0a531409ec2d36a54fcca16700ceb6bc6e6ec
MdAbdulKayumKhan <kayum261@gmail.com>
2021-10-13 03:19:05 +0600

    added from public repo

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBjNuMJRo1Z8Zfq3I42CdCQK3ya2kM0JN4",
+    authDomain: "online-exam-5e607.firebaseapp.com",
+    projectId: "online-exam-5e607",
+    storageBucket: "online-exam-5e607.appspot.com",
+    messagingSenderId: "235885787417",
+    appId: "1:235885787417:web:cbcf893e83c478491c6d88"
+  };
+
+  export default firebaseConfig;
